e4a1fdd2d5e89ef41be81bd1e7d9feffc706297e
Nathaniel Daley <ndaley64@gmail.com>
2024-08-26 12:39:29 -0500

    Add scripts folder and index.js to source control

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "./images/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "./images/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "./images/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest canopy",
+    link: "./images/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "./images/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "./images/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
